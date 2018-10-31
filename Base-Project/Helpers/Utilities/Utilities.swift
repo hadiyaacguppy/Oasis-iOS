330936cfd85f426fc4870fdafabde2cd85088807
Mhmd Rizk <mhmdrizk94@outlook.com>
2018-10-31 15:49:19 +0200

    added show activty  controller

--- Base-Project/Helpers/Utilities/Utilities.swift
+++ Base-Project/Helpers/Utilities/Utilities.swift
@@ -129,6 +129,13 @@ struct Utilities  {
             }
         }
         
+        static func showActivityController(withLinkToShare link : String ,andWithPresenter presenter: UIViewController) {
+            let activityItems = [link]
+            let activityController = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+            presenter.present(activityController, animated: true, completion: nil)
+        }
+      
+        
     }
     
     
