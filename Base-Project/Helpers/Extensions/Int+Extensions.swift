fceae53af644fd3a5cf1bd5237360d61c2bd0c0f
Wassim  Seifeddine <wassim_seifeddine95@live.com>
2018-11-28 14:45:03 +0200

    Readded timeparts

--- Base-Project/Helpers/Extensions/Int+Extensions.swift
+++ Base-Project/Helpers/Extensions/Int+Extensions.swift
@@ -44,3 +44,37 @@ extension Int {
     }
    
 }
+struct TimeParts: CustomStringConvertible {
+    var seconds = 0
+    var minutes = 0
+    /// The string representation of the time parts (ex: 07:37)
+    var description: String {
+        return NSString(format: "%02d:%02d", minutes, seconds) as String
+    }
+    var done : Bool {
+        return seconds == 0 && minutes ==  0
+    }
+}
+
+
+
+extension Int {
+    /// The time parts for this integer represented from total seconds in time.
+    /// -- returns: A TimeParts struct that describes the parts of time
+    func toTimeParts() -> TimeParts {
+        let seconds = self
+        var mins = 0
+        var secs = seconds
+        if seconds >= 60 {
+            mins = Int(seconds / 60)
+            secs = seconds - (mins * 60)
+        }
+        
+        return TimeParts(seconds: secs, minutes: mins)
+    }
+    
+    /// The string representation of the time parts (ex: 07:37)
+    func asTimeString() -> String {
+        return toTimeParts().description
+    }
+}
