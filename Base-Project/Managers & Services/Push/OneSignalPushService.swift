bd285e2a50d6d223470ac7cb4b29767a3b043ccd
Wassim  Seifeddine <wassim_seifeddine95@live.com>
2019-01-15 11:02:46 +0200

    Fixed build errors

--- Base-Project/Managers & Services/Push/OneSignalPushService.swift
+++ Base-Project/Managers & Services/Push/OneSignalPushService.swift
@@ -12,27 +12,6 @@ import UIKit
 import OneSignal
 import RxSwift
 
-struct NotificationPayload  {
-    
-    /* Notification Payload */
-    public private(set) var payload : OSNotificationPayload?
-    
-    /* Set to true when the user was able to see the notification and reacted to it
-     Set to false when app is in focus and in-app alerts are disabled, or the remote notification is silent. */
-    public private(set) var wasShown : Bool?
-    
-    
-    
-    /* Set to true if the app was in focus when the notification  */
-    public private(set) var wasAppInFocus : Bool?
-    
-    /* Set to true when the received notification is silent
-     Silent means there is no alert, sound, or badge payload in the aps dictionary
-     requires remote-notification within UIBackgroundModes array of the Info.plist */
-    public private(set) var isSilentNotification : Bool?
-    
-}
-
 final class OneSignalPushService: NSObject{
     
     
