c1f6d70af306225a3756c563028e5d2ee1c3356b
Wassim  Seifeddine <wassim_seifeddine95@live.com>
2019-03-26 18:19:24 +0200

    Double extensions made clearer. Removed romanString function from Int+Extensions

--- Base-Project/Helpers/Extensions/Double+Extensions.swift
+++ Base-Project/Helpers/Extensions/Double+Extensions.swift
@@ -10,30 +10,26 @@ import Foundation
 
 
 extension Double {
-    /**
-     Better on the eyes from self = self - 1 or the warning from --
-     */
-    mutating func decrement(){
-        self =  self - 1
-    }
     
+    /// Removes all the trailing zeros in the Number
+    ///
+    /// - Returns: String with all zeros removed
     func stringByRemoveTrailingZero() -> String{
         
         return String(format: "%g", self)
         
     }
+    
+    /// Formats the Double
+    ///
+    /// - Parameter f: the format that you want. %.1f based on how many digit after the comma (,) you want
+    /// - Returns: String representing the double formatted
     func format(f: String) -> String {
         return String(format: "%\(f)f", self)
     }
     
-    /**
-     Returns the Double rounded with n digits after the decimal
-     */
-    func rounded(toPlaces places:Int) -> Double {
-        let divisor = pow(10.0, Double(places))
-        return (self * divisor).rounded() / divisor
-    }
     
+    /// Assuming that the double is a timestamp, this functions would return it as a string
     var dateFormatted : String? {
         let date = Date(timeIntervalSince1970: self)
         let dateFormatter = DateFormatter()
@@ -41,12 +37,15 @@ extension Double {
         dateFormatter.dateStyle = DateFormatter.Style.short //Set date style
         return dateFormatter.string(from: date)
     }
-    func dateFormatted(withFormat format : String) -> String{
+    
+    /// Assuming that the double is a timestamp, this functions would return it as a string
+    ///
+    /// - Parameter format: The format you want the Dateformatter to use. check DateFormats in Constants file
+    /// - Returns: String representing the date 
+    func dateFormatted(withFormat format : DateFormats) -> String{
         let date = Date(timeIntervalSince1970: self)
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = format
-        
-        
+        dateFormatter.dateFormat = format.rawValue
         return dateFormatter.string(from: date)
     }
     
