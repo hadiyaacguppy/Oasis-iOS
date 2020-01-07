de2ff166d38a179c18770c11cb54cec5f1121a9a
mojtaba almoussawi <mojtaba.almoussawi@tedmob.com>
2020-01-07 10:17:10 +0200

    UISearchBar reactive textChanged property removed

--- Base-Project/Helpers/Extensions/Rx+Extensions/Reactive+Validation.swift
+++ Base-Project/Helpers/Extensions/Rx+Extensions/Reactive+Validation.swift
@@ -12,16 +12,6 @@ import RxCocoa
 import UIKit
 
 /// Extension Usage:
-/// This is to add the ability for checking for UISearchBar.text even if changes was'nt done by user - Keyboard input
-/// For cases when text property is edited from code, picker etc..
-extension Reactive where Base: UISearchBar {
-    var textChanged : Observable<String?>{
-        return Observable.merge(self.base.rx.observe(String.self, "text"),
-                                self.base.rx.text.asObservable() )
-    }
-}
-
-/// Extension Usage:
 /// This is to add the ability for checking for UITextField.text even if changes was'nt done by user - Keyboard input
 /// For cases when text property is edited from code, picker etc..
 extension Reactive where Base: UITextField {
--- R.generated.swift
+++ R.generated.swift
@@ -11,25 +11,157 @@ import UIKit
 struct R: Rswift.Validatable {
   fileprivate static let applicationLocale = hostingBundle.preferredLocalizations.first.flatMap(Locale.init) ?? Locale.current
   fileprivate static let hostingBundle = Bundle(for: R.Class.self)
-  
+
+  /// Find first language and bundle for which the table exists
+  fileprivate static func localeBundle(tableName: String, preferredLanguages: [String]) -> (Foundation.Locale, Foundation.Bundle)? {
+    // Filter preferredLanguages to localizations, use first locale
+    var languages = preferredLanguages
+      .map(Locale.init)
+      .prefix(1)
+      .flatMap { locale -> [String] in
+        if hostingBundle.localizations.contains(locale.identifier) {
+          if let language = locale.languageCode, hostingBundle.localizations.contains(language) {
+            return [locale.identifier, language]
+          } else {
+            return [locale.identifier]
+          }
+        } else if let language = locale.languageCode, hostingBundle.localizations.contains(language) {
+          return [language]
+        } else {
+          return []
+        }
+      }
+
+    // If there's no languages, use development language as backstop
+    if languages.isEmpty {
+      if let developmentLocalization = hostingBundle.developmentLocalization {
+        languages = [developmentLocalization]
+      }
+    } else {
+      // Insert Base as second item (between locale identifier and languageCode)
+      languages.insert("Base", at: 1)
+
+      // Add development language as backstop
+      if let developmentLocalization = hostingBundle.developmentLocalization {
+        languages.append(developmentLocalization)
+      }
+    }
+
+    // Find first language for which table exists
+    // Note: key might not exist in chosen language (in that case, key will be shown)
+    for language in languages {
+      if let lproj = hostingBundle.url(forResource: language, withExtension: "lproj"),
+         let lbundle = Bundle(url: lproj)
+      {
+        let strings = lbundle.url(forResource: tableName, withExtension: "strings")
+        let stringsdict = lbundle.url(forResource: tableName, withExtension: "stringsdict")
+
+        if strings != nil || stringsdict != nil {
+          return (Locale(identifier: language), lbundle)
+        }
+      }
+    }
+
+    // If table is available in main bundle, don't look for localized resources
+    let strings = hostingBundle.url(forResource: tableName, withExtension: "strings", subdirectory: nil, localization: nil)
+    let stringsdict = hostingBundle.url(forResource: tableName, withExtension: "stringsdict", subdirectory: nil, localization: nil)
+
+    if strings != nil || stringsdict != nil {
+      return (applicationLocale, hostingBundle)
+    }
+
+    // If table is not found for requested languages, key will be shown
+    return nil
+  }
+
+  /// Load string from Info.plist file
+  fileprivate static func infoPlistString(path: [String], key: String) -> String? {
+    var dict = hostingBundle.infoDictionary
+    for step in path {
+      guard let obj = dict?[step] as? [String: Any] else { return nil }
+      dict = obj
+    }
+    return dict?[key] as? String
+  }
+
   static func validate() throws {
     try intern.validate()
   }
-  
+
+  #if os(iOS) || os(tvOS)
+  /// This `R.storyboard` struct is generated, and contains static references to 5 storyboards.
+  struct storyboard {
+    /// Storyboard `BaseWebView`.
+    static let baseWebView = _R.storyboard.baseWebView()
+    /// Storyboard `ChangePassword`.
+    static let changePassword = _R.storyboard.changePassword()
+    /// Storyboard `Initial`.
+    static let initial = _R.storyboard.initial()
+    /// Storyboard `LaunchScreen`.
+    static let launchScreen = _R.storyboard.launchScreen()
+    /// Storyboard `PinVerification`.
+    static let pinVerification = _R.storyboard.pinVerification()
+
+    #if os(iOS) || os(tvOS)
+    /// `UIStoryboard(name: "BaseWebView", bundle: ...)`
+    static func baseWebView(_: Void = ()) -> UIKit.UIStoryboard {
+      return UIKit.UIStoryboard(resource: R.storyboard.baseWebView)
+    }
+    #endif
+
+    #if os(iOS) || os(tvOS)
+    /// `UIStoryboard(name: "ChangePassword", bundle: ...)`
+    static func changePassword(_: Void = ()) -> UIKit.UIStoryboard {
+      return UIKit.UIStoryboard(resource: R.storyboard.changePassword)
+    }
+    #endif
+
+    #if os(iOS) || os(tvOS)
+    /// `UIStoryboard(name: "Initial", bundle: ...)`
+    static func initial(_: Void = ()) -> UIKit.UIStoryboard {
+      return UIKit.UIStoryboard(resource: R.storyboard.initial)
+    }
+    #endif
+
+    #if os(iOS) || os(tvOS)
+    /// `UIStoryboard(name: "LaunchScreen", bundle: ...)`
+    static func launchScreen(_: Void = ()) -> UIKit.UIStoryboard {
+      return UIKit.UIStoryboard(resource: R.storyboard.launchScreen)
+    }
+    #endif
+
+    #if os(iOS) || os(tvOS)
+    /// `UIStoryboard(name: "PinVerification", bundle: ...)`
+    static func pinVerification(_: Void = ()) -> UIKit.UIStoryboard {
+      return UIKit.UIStoryboard(resource: R.storyboard.pinVerification)
+    }
+    #endif
+
+    fileprivate init() {}
+  }
+  #endif
+
+  /// This `R.entitlements` struct is generated, and contains static references to 1 properties.
+  struct entitlements {
+    static let apsEnvironment = infoPlistString(path: [], key: "aps-environment") ?? "development"
+
+    fileprivate init() {}
+  }
+
   /// This `R.file` struct is generated, and contains static references to 1 files.
   struct file {
     /// Resource file `Countries.bundle`.
     static let countriesBundle = Rswift.FileResource(bundle: R.hostingBundle, name: "Countries", pathExtension: "bundle")
-    
+
     /// `bundle.url(forResource: "Countries", withExtension: "bundle")`
     static func countriesBundle(_: Void = ()) -> Foundation.URL? {
       let fileResource = R.file.countriesBundle
       return fileResource.bundle.url(forResource: fileResource)
     }
-    
+
     fileprivate init() {}
   }
-  
+
   /// This `R.image` struct is generated, and contains static references to 7 images.
   struct image {
     /// Image `NavBackAr`.
@@ -46,216 +178,213 @@ struct R: Rswift.Validatable {
     static let error = Rswift.ImageResource(bundle: R.hostingBundle, name: "error")
     /// Image `offline`.
     static let offline = Rswift.ImageResource(bundle: R.hostingBundle, name: "offline")
-    
+
+    #if os(iOS) || os(tvOS)
     /// `UIImage(named: "NavBack", bundle: ..., traitCollection: ...)`
     static func navBack(compatibleWith traitCollection: UIKit.UITraitCollection? = nil) -> UIKit.UIImage? {
       return UIKit.UIImage(resource: R.image.navBack, compatibleWith: traitCollection)
     }
-    
+    #endif
+
+    #if os(iOS) || os(tvOS)
     /// `UIImage(named: "NavBackAr", bundle: ..., traitCollection: ...)`
     static func navBackAr(compatibleWith traitCollection: UIKit.UITraitCollection? = nil) -> UIKit.UIImage? {
       return UIKit.UIImage(resource: R.image.navBackAr, compatibleWith: traitCollection)
     }
-    
+    #endif
+
+    #if os(iOS) || os(tvOS)
     /// `UIImage(named: "NavClose", bundle: ..., traitCollection: ...)`
     static func navClose(compatibleWith traitCollection: UIKit.UITraitCollection? = nil) -> UIKit.UIImage? {
       return UIKit.UIImage(resource: R.image.navClose, compatibleWith: traitCollection)
     }
-    
+    #endif
+
+    #if os(iOS) || os(tvOS)
     /// `UIImage(named: "RegistrationNotView", bundle: ..., traitCollection: ...)`
     static func registrationNotView(compatibleWith traitCollection: UIKit.UITraitCollection? = nil) -> UIKit.UIImage? {
       return UIKit.UIImage(resource: R.image.registrationNotView, compatibleWith: traitCollection)
     }
-    
+    #endif
+
+    #if os(iOS) || os(tvOS)
     /// `UIImage(named: "RegistrationView", bundle: ..., traitCollection: ...)`
     static func registrationView(compatibleWith traitCollection: UIKit.UITraitCollection? = nil) -> UIKit.UIImage? {
       return UIKit.UIImage(resource: R.image.registrationView, compatibleWith: traitCollection)
     }
-    
+    #endif
+
+    #if os(iOS) || os(tvOS)
     /// `UIImage(named: "error", bundle: ..., traitCollection: ...)`
     static func error(compatibleWith traitCollection: UIKit.UITraitCollection? = nil) -> UIKit.UIImage? {
       return UIKit.UIImage(resource: R.image.error, compatibleWith: traitCollection)
     }
-    
+    #endif
+
+    #if os(iOS) || os(tvOS)
     /// `UIImage(named: "offline", bundle: ..., traitCollection: ...)`
     static func offline(compatibleWith traitCollection: UIKit.UITraitCollection? = nil) -> UIKit.UIImage? {
       return UIKit.UIImage(resource: R.image.offline, compatibleWith: traitCollection)
     }
-    
+    #endif
+
     fileprivate init() {}
   }
-  
-  /// This `R.storyboard` struct is generated, and contains static references to 5 storyboards.
-  struct storyboard {
-    /// Storyboard `BaseWebView`.
-    static let baseWebView = _R.storyboard.baseWebView()
-    /// Storyboard `ChangePassword`.
-    static let changePassword = _R.storyboard.changePassword()
-    /// Storyboard `Initial`.
-    static let initial = _R.storyboard.initial()
-    /// Storyboard `LaunchScreen`.
-    static let launchScreen = _R.storyboard.launchScreen()
-    /// Storyboard `PinVerification`.
-    static let pinVerification = _R.storyboard.pinVerification()
-    
-    /// `UIStoryboard(name: "BaseWebView", bundle: ...)`
-    static func baseWebView(_: Void = ()) -> UIKit.UIStoryboard {
-      return UIKit.UIStoryboard(resource: R.storyboard.baseWebView)
-    }
-    
-    /// `UIStoryboard(name: "ChangePassword", bundle: ...)`
-    static func changePassword(_: Void = ()) -> UIKit.UIStoryboard {
-      return UIKit.UIStoryboard(resource: R.storyboard.changePassword)
-    }
-    
-    /// `UIStoryboard(name: "Initial", bundle: ...)`
-    static func initial(_: Void = ()) -> UIKit.UIStoryboard {
-      return UIKit.UIStoryboard(resource: R.storyboard.initial)
-    }
-    
-    /// `UIStoryboard(name: "LaunchScreen", bundle: ...)`
-    static func launchScreen(_: Void = ()) -> UIKit.UIStoryboard {
-      return UIKit.UIStoryboard(resource: R.storyboard.launchScreen)
-    }
-    
-    /// `UIStoryboard(name: "PinVerification", bundle: ...)`
-    static func pinVerification(_: Void = ()) -> UIKit.UIStoryboard {
-      return UIKit.UIStoryboard(resource: R.storyboard.pinVerification)
-    }
-    
-    fileprivate init() {}
-  }
-  
+
   fileprivate struct intern: Rswift.Validatable {
     fileprivate static func validate() throws {
       try _R.validate()
     }
-    
+
     fileprivate init() {}
   }
-  
+
   fileprivate class Class {}
-  
+
   fileprivate init() {}
 }
 
 struct _R: Rswift.Validatable {
   static func validate() throws {
+    #if os(iOS) || os(tvOS)
     try storyboard.validate()
+    #endif
   }
-  
+
+  #if os(iOS) || os(tvOS)
   struct storyboard: Rswift.Validatable {
     static func validate() throws {
+      #if os(iOS) || os(tvOS)
       try baseWebView.validate()
+      #endif
+      #if os(iOS) || os(tvOS)
       try changePassword.validate()
+      #endif
+      #if os(iOS) || os(tvOS)
       try initial.validate()
+      #endif
+      #if os(iOS) || os(tvOS)
       try launchScreen.validate()
+      #endif
+      #if os(iOS) || os(tvOS)
       try pinVerification.validate()
+      #endif
     }
-    
+
+    #if os(iOS) || os(tvOS)
     struct baseWebView: Rswift.StoryboardResourceWithInitialControllerType, Rswift.Validatable {
       typealias InitialController = BaseWebViewViewController
-      
+
       let baseWebViewViewControllerVC = StoryboardViewControllerResource<BaseWebViewViewController>(identifier: "BaseWebViewViewControllerVC")
       let bundle = R.hostingBundle
       let name = "BaseWebView"
-      
+
       func baseWebViewViewControllerVC(_: Void = ()) -> BaseWebViewViewController? {
         return UIKit.UIStoryboard(resource: self).instantiateViewController(withResource: baseWebViewViewControllerVC)
       }
-      
+
       static func validate() throws {
-        if #available(iOS 11.0, *) {
+        if #available(iOS 11.0, tvOS 11.0, *) {
         }
         if _R.storyboard.baseWebView().baseWebViewViewControllerVC() == nil { throw Rswift.ValidationError(description:"[R.swift] ViewController with identifier 'baseWebViewViewControllerVC' could not be loaded from storyboard 'BaseWebView' as 'BaseWebViewViewController'.") }
       }
-      
+
       fileprivate init() {}
     }
-    
+    #endif
+
+    #if os(iOS) || os(tvOS)
     struct changePassword: Rswift.StoryboardResourceWithInitialControllerType, Rswift.Validatable {
       typealias InitialController = UIKit.UINavigationController
-      
+
       let bundle = R.hostingBundle
       let changePasswordViewControllerTableVC = StoryboardViewControllerResource<ChangePasswordViewController>(identifier: "ChangePasswordViewControllerTableVC")
       let name = "ChangePassword"
-      
+
       func changePasswordViewControllerTableVC(_: Void = ()) -> ChangePasswordViewController? {
         return UIKit.UIStoryboard(resource: self).instantiateViewController(withResource: changePasswordViewControllerTableVC)
       }
-      
+
       static func validate() throws {
         if UIKit.UIImage(named: "RegistrationNotView", in: R.hostingBundle, compatibleWith: nil) == nil { throw Rswift.ValidationError(description: "[R.swift] Image named 'RegistrationNotView' is used in storyboard 'ChangePassword', but couldn't be loaded.") }
-        if #available(iOS 11.0, *) {
+        if #available(iOS 11.0, tvOS 11.0, *) {
         }
         if _R.storyboard.changePassword().changePasswordViewControllerTableVC() == nil { throw Rswift.ValidationError(description:"[R.swift] ViewController with identifier 'changePasswordViewControllerTableVC' could not be loaded from storyboard 'ChangePassword' as 'ChangePasswordViewController'.") }
       }
-      
+
       fileprivate init() {}
     }
-    
+    #endif
+
+    #if os(iOS) || os(tvOS)
     struct initial: Rswift.StoryboardResourceWithInitialControllerType, Rswift.Validatable {
       typealias InitialController = InitialViewController
-      
+
       let bundle = R.hostingBundle
       let initialViewControllerVC = StoryboardViewControllerResource<InitialViewController>(identifier: "InitialViewControllerVC")
       let name = "Initial"
-      
+
       func initialViewControllerVC(_: Void = ()) -> InitialViewController? {
         return UIKit.UIStoryboard(resource: self).instantiateViewController(withResource: initialViewControllerVC)
       }
-      
+
       static func validate() throws {
-        if #available(iOS 11.0, *) {
+        if #available(iOS 11.0, tvOS 11.0, *) {
         }
         if _R.storyboard.initial().initialViewControllerVC() == nil { throw Rswift.ValidationError(description:"[R.swift] ViewController with identifier 'initialViewControllerVC' could not be loaded from storyboard 'Initial' as 'InitialViewController'.") }
       }
-      
+
       fileprivate init() {}
     }
-    
+    #endif
+
+    #if os(iOS) || os(tvOS)
     struct launchScreen: Rswift.StoryboardResourceWithInitialControllerType, Rswift.Validatable {
       typealias InitialController = UIKit.UIViewController
-      
+
       let bundle = R.hostingBundle
       let name = "LaunchScreen"
-      
+
       static func validate() throws {
-        if #available(iOS 11.0, *) {
+        if #available(iOS 11.0, tvOS 11.0, *) {
         }
       }
-      
+
       fileprivate init() {}
     }
-    
+    #endif
+
+    #if os(iOS) || os(tvOS)
     struct pinVerification: Rswift.StoryboardResourceWithInitialControllerType, Rswift.Validatable {
       typealias InitialController = BaseNavigationController
-      
+
       let bundle = R.hostingBundle
       let name = "PinVerification"
       let pinVerificationNav = StoryboardViewControllerResource<BaseNavigationController>(identifier: "pinVerificationNav")
       let pinVerificationViewControllerTableVC = StoryboardViewControllerResource<PinVerificationViewController>(identifier: "PinVerificationViewControllerTableVC")
-      
+
       func pinVerificationNav(_: Void = ()) -> BaseNavigationController? {
         return UIKit.UIStoryboard(resource: self).instantiateViewController(withResource: pinVerificationNav)
       }
-      
+
       func pinVerificationViewControllerTableVC(_: Void = ()) -> PinVerificationViewController? {
         return UIKit.UIStoryboard(resource: self).instantiateViewController(withResource: pinVerificationViewControllerTableVC)
       }
-      
+
       static func validate() throws {
-        if #available(iOS 11.0, *) {
+        if #available(iOS 11.0, tvOS 11.0, *) {
         }
         if _R.storyboard.pinVerification().pinVerificationViewControllerTableVC() == nil { throw Rswift.ValidationError(description:"[R.swift] ViewController with identifier 'pinVerificationViewControllerTableVC' could not be loaded from storyboard 'PinVerification' as 'PinVerificationViewController'.") }
         if _R.storyboard.pinVerification().pinVerificationNav() == nil { throw Rswift.ValidationError(description:"[R.swift] ViewController with identifier 'pinVerificationNav' could not be loaded from storyboard 'PinVerification' as 'BaseNavigationController'.") }
       }
-      
+
       fileprivate init() {}
     }
-    
+    #endif
+
     fileprivate init() {}
   }
-  
+  #endif
+
   fileprivate init() {}
 }
