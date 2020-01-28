addb2016403a57471d07f4b1b924311f25eeee21
HADI <hadi.yaacoub@tedmob.com>
2020-01-28 14:09:22 +0200

    Added 2 functions to String+Extensions

--- Base-Project/Helpers/Extensions/String+Extensions.swift
+++ Base-Project/Helpers/Extensions/String+Extensions.swift
@@ -25,7 +25,7 @@ extension String {
     var localized: String {
         return NSLocalizedString(self, comment: "")
     }
-
+    
     /// Compares 2 strings without case sensitivity
     ///
     /// - parameter otherString: The other string to compare
@@ -166,8 +166,47 @@ extension String {
     public var hasNumbers: Bool {
         return rangeOfCharacter(from: .decimalDigits, options: .literal, range: nil) != nil
     }
-    
-
+    /*
+     Function that checks for a valid lebanese number
+     This is to be used in case not using a phone number validator and the app for example have only lebanese numbers
+     For 03 numbers, the case of 03 or 3 is handled
+     */
+    public
+    func isValidLebaneseNumber() -> Bool{
+        let first2 = String(self.prefix(2))
+        let first1 = String(self.prefix(1))
+        if first1 == "3", self.count == 7{
+            return true
+        }
+        
+        guard first2 == "03" || first2 == "70" || first2 == "71" || first2 == "76" || first2 == "78" || first2 == "79" || first2 == "81" || first2 == "82" else {
+            return false
+        }
+        
+        guard self.count == 8 else {
+            return false
+        }
+        
+        return true
+    }
+    /*
+     This function returns a string with a formatted number with commas eg: 65,000 OR 124,927,250
+     Usually use this function for prices, points etc..
+     */
+    public
+    func formatNumberWithCommas() -> String{
+        var stringToReturn = ""
+        let integerNumber = Int(self)
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        
+        if let num = integerNumber {
+            stringToReturn = numberFormatter.string(from: NSNumber(value: num))!
+        }else{
+            stringToReturn = ""
+        }
+        return stringToReturn
+    }
 }
 
 extension Optional where Wrapped == String{
@@ -209,6 +248,6 @@ extension Optional where Wrapped == String{
         }
         return attr
     }
-   
-   
+    
+    
 }
