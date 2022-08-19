da7d9b4649532e1a83dfa0dd33820af373a7be68
ali.swaidan <ali.swaidan@tedmob.com>
2022-08-19 17:32:26 +0300

    Added App Uses Non-Exempt Encryption Key to info.plist & Updated AppDelegate+OneSignal

--- Base-Project/Supporting Files/App Delegate/AppDelegate+OneSignal.swift
+++ Base-Project/Supporting Files/App Delegate/AppDelegate+OneSignal.swift
@@ -7,16 +7,20 @@
 //
 
 import UIKit
+import SessionRepository
 
 extension AppDelegate {
     
-    func initOneSignal(withLaunchOption launchOptions: [UIApplication.LaunchOptionsKey: Any]?, andOneSignalId oneSignalID: String) {
-        OneSignalPushService.shared.initializeOneSignal(withLaunchOptions: launchOptions, andAppID: oneSignalAppId)
+    func initOneSignal(withLaunchOption launchOptions: [UIApplication.LaunchOptionsKey: Any]?,
+                       andOneSignalId oneSignalID: String) {
+        OneSignalPushService.shared.initializeOneSignal(withLaunchOptions: launchOptions,
+                                                        andAppID: oneSignalID)
         OneSignalPushService.shared.playerIdDidChange = { token in
+            guard SessionRepository.shared.token != nil else {
+                return
+            }
             _ = APIClient.shared.setOneSignalToken(withToken: token)
                 .subscribe()
-            
         }
     }
-    
 }
