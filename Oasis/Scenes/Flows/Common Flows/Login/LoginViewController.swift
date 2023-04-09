ed6eb976efc006b43a5b24db46e62107c9c08511
Hadi Yaac <hadi.jacob@gmail.com>
2023-04-09 17:58:10 +0300

    Login scene almost done

--- Oasis/Scenes/Flows/Common Flows/Login/LoginViewController.swift
+++ Oasis/Scenes/Flows/Common Flows/Login/LoginViewController.swift
@@ -26,6 +26,67 @@ class LoginViewController: BaseViewController {
         imageV.contentMode = .scaleAspectFill
         return imageV
     }()
+    
+    private lazy var mainStackView = {
+        UIStackView()
+            .axis(.vertical)
+            .spacing(40)
+            .autoLayout()
+            .distributionMode(.fill)
+    }()
+    
+    private lazy var emailStackview = {
+        UIStackView()
+            .axis(.vertical)
+            .spacing(4)
+            .autoLayout()
+    }()
+    
+    private lazy var passwordStackview = {
+        UIStackView()
+            .axis(.vertical)
+            .spacing(4)
+            .autoLayout()
+    }()
+    
+    private lazy var emailLabel : BaseLabel = {
+        let label = BaseLabel()
+        label.font = MainFont.medium.with(size: 22)
+        label.textColor = .white
+        label.numberOfLines = 2
+        label.text = "Email".localized
+        return label
+    }()
+    
+    private lazy var passwordLabel : BaseLabel = {
+        let label = BaseLabel()
+        label.font = MainFont.medium.with(size: 22)
+        label.textColor = .white
+        label.numberOfLines = 2
+        label.text = "Password".localized
+        return label
+    }()
+    
+    private lazy var emailTextfield : WhiteBorderTextfield = {
+        let txtf = WhiteBorderTextfield()
+        txtf.keyboardType = .emailAddress
+        return txtf
+    }()
+    
+    private lazy var passwordTextfield : WhiteBorderTextfield = {
+        let txtf = WhiteBorderTextfield()
+        txtf.isSecureTextEntry = true
+        return txtf
+    }()
+    
+    private lazy var loginButton : WhiteBorderButton = {
+        let button = WhiteBorderButton()
+        button.setTitle("Login".localized, for: .normal)
+        button.onTap {
+            
+        }
+        return button
+    }()
 }
 
 //MARK:- View Lifecycle
@@ -53,6 +114,10 @@ extension LoginViewController{
     fileprivate
     func setupUI(){
         addBackgroundImage()
+        buildStacks()
+        buildEmailStack()
+        buildPasswordStack()
+        addLoginButton()
     }
     
     private func addBackgroundImage(){
@@ -62,6 +127,42 @@ extension LoginViewController{
             backgroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             backgroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        ])
+    }
+    
+    private func buildStacks(){
+        view.addSubview(mainStackView)
+        NSLayoutConstraint.activate([
+            mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
+            mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+            //mainStackView.bottomAnchor.constraint(equalTo: nextButton.topAnchor, constant: 8)
+        ])
+        mainStackView.addArrangedSubview(emailStackview)
+        mainStackView.addArrangedSubview(passwordStackview)
+    }
+    
+    private func buildEmailStack(){
+        emailStackview.addArrangedSubview(emailLabel)
+        emailStackview.addArrangedSubview(emailTextfield)
+        
+        emailTextfield.heightAnchor.constraint(equalToConstant: 68).isActive = true
+    }
+    
+    private func buildPasswordStack(){
+        passwordStackview.addArrangedSubview(passwordLabel)
+        passwordStackview.addArrangedSubview(passwordTextfield)
+        
+        passwordTextfield.heightAnchor.constraint(equalToConstant: 68).isActive = true
+    }
+    
+    private func addLoginButton(){
+        view.addSubview(loginButton)
+        NSLayoutConstraint.activate([
+            loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 43),
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginButton.heightAnchor.constraint(equalToConstant: 58),
+            loginButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -66)
         ])
     }
 }
